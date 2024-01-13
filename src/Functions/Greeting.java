23b37cfc73e1081b2cc1a1fad9dad204af999547
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-01-14 00:00:12 +0530

    greet message using function

---
+++ src/Functions/Greeting.java
@@ -0,0 +1,14 @@
+package Functions;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String result = greet();
+        System.out.println(result);
+    }
+
+    static String greet(){
+String greeting = "Namaste";
+return greeting;
+
+    }
+}
