2e027759229572edfbb940cb4b80ef303581ddb8
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-03-08 21:11:49 +0530

    :memo:smallest character in letters that is lexicographically greater than target.

---
+++ src/BinarySearch/SmallestLetter.java
@@ -0,0 +1,48 @@
+package BinarySearch;
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+public class SmallestLetter {
+    public static void main(String[] args) {
+        char[]letters = {'c','f','j'};
+        System.out.println(nextGreatestLetter(letters,'c'));
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        // If start is out of bounds, return the first character.
+        if (start >= letters.length) {
+            return letters[0];
+        }
+        return letters[start];
+    }
+}
+// You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+// Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+ 
+
+// Example 1:
+
+// Input: letters = ["c","f","j"], target = "a"
+// Output: "c"
+// Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+// Example 2:
+
+// Input: letters = ["c","f","j"], target = "c"
+// Output: "f"
+// Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+
+// Example 3:
+
+// Input: letters = ["x","x","y","y"], target = "z"
+// Output: "x"
+// Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
