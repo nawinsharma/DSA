00d21e44d4e4d7380d6e4c868da999ea10f398f7
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-04-02 00:42:41 +0530

    :sparkles:find all Duplicates in an array

---
+++ src/CyclicSort_LEETCODE/findAllDuplicates.java
@@ -0,0 +1,37 @@
+package CyclicSort_LEETCODE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class findAllDuplicates {
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+        List<Integer> duplicates = findDuplicate(arr);
+        System.out.println("Duplicates numbers: " + duplicates);
+    }
+
+    public static List<Integer> findDuplicate(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
