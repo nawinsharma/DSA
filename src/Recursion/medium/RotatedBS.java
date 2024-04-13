c605a2bed9c004d7a4bfea25524a9aa5a2e04fef
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-04-13 22:21:32 +0530

    :sparkles: RBS  using recursion

---
+++ src/Recursion/medium/RotatedBS.java
@@ -0,0 +1,33 @@
+package Recursion.medium;
+
+public class RotatedBS {
+    public static void main(String[] args) {
+        // arr = {5,6,7,8,1,2,3}
+        int[] arr = {5,6,7,8,1,2,3};
+        System.out.println(search(arr, 2, 0, arr.length-1));
+    }
+
+    static int search(int[] arr, int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int m = s + (e - s) / 2;
+        if (arr[m] == target) {
+            return m;
+        }
+        if (arr[s] <= arr[m]) {
+            if (target >= arr[s] && target <= arr[m]) {
+                // search e=m-1
+                return search(arr, target, s, m - 1);
+            } else {
+                // s = m+1
+                return search(arr, target, m + 1, e);
+            }
+        }
+        if (target >= arr[m] && target <= arr[e]) {
+            return search(arr, target, m + 1, e);
+        }
+        return search(arr, target, s, m - 1);
+
+    }
+}
