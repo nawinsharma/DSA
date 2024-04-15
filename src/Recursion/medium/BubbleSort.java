de7882170e896c7e76394b81e15827aca4ba3e6b
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-04-15 23:16:08 +0530

    :sparkles: bubble sort  using recursion

---
+++ src/Recursion/medium/BubbleSort.java
@@ -0,0 +1,28 @@
+package Recursion.medium;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] nums = { 5, 4, 3, 2, 1 };
+        sort(nums, nums.length - 1, 0);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    static void sort(int[] arr, int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            if (arr[c] > arr[c + 1]) {
+                // swap
+                int temp = arr[c];
+                arr[c] = arr[c + 1];
+                arr[c + 1] = temp;
+            }
+            sort(arr, r, c + 1);
+        } else {
+            sort(arr, r - 1, 0);
+        }
+    }
+}
