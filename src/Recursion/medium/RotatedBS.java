074d2d120f4627f658f7db02846e0c8b25779ab0
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-16 02:27:45 +0530

    :sparkles:Rotated binary search using recursion

--- src/Recursion/medium/RotatedBS.java
+++ src/Recursion/medium/RotatedBS.java
@@ -3,10 +3,33 @@ package Recursion.medium;
 public class RotatedBS {
     public static void main(String[] args) {
         // arr = {5,6,7,8,1,2,3}
-        int[] arr = {5,6,7,8,1,2,3};
-        System.out.println(search(arr, 2, 0, arr.length-1));
+        int[] arr = { 5, 6, 7, 8, 1, 2, 3 };
+        System.out.println(search(arr, 2, 0, arr.length - 1));
     }
 
+    // static int search(int[] arr, int target, int s, int e) {
+    // if (s > e) {
+    // return -1;
+    // }
+    // int m = s + (e - s) / 2;
+    // if (arr[m] == target) {
+    // return m;
+    // }
+    // if (arr[s] <= arr[m]) {
+    // if (target >= arr[s] && target <= arr[m]) {
+    // // search e=m-1
+    // return search(arr, target, s, m - 1);
+    // } else {
+    // // s = m+1
+    // return search(arr, target, m + 1, e);
+    // }
+    // }
+    // if (target >= arr[m] && target <= arr[e]) {
+    // return search(arr, target, m + 1, e);
+    // }
+    // return search(arr, target, s, m - 1);
+
+    // }
     static int search(int[] arr, int target, int s, int e) {
         if (s > e) {
             return -1;
@@ -16,18 +39,17 @@ public class RotatedBS {
             return m;
         }
         if (arr[s] <= arr[m]) {
-            if (target >= arr[s] && target <= arr[m]) {
-                // search e=m-1
+            if (arr[s] <= target && arr[e] >= target) {
                 return search(arr, target, s, m - 1);
             } else {
-                // s = m+1
                 return search(arr, target, m + 1, e);
             }
-        }
-        if (target >= arr[m] && target <= arr[e]) {
-            return search(arr, target, m + 1, e);
-        }
-        return search(arr, target, s, m - 1);
 
+         }
+        if (target > arr[m] && target < arr[e]) {
+            return search(arr, target, m + 1, e);
+        } else {
+            return search(arr, target, s, m - 1);
+        }
     }
 }
