852aac78fc74b81d57bfecf11657378e9a1d9f45
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-23 00:49:34 +0530

    :sparkles: matrix diagonal sum problem

---
+++ src/Arrays/MatrixDiagonalSum.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class MatrixDiagonalSum {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println(diagonalStudy(matrix));
+    }
+    public static int diagonalStudy(int[][] matrix){
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int sum = 0;
+        for(int rowNo = 0; rowNo < r; rowNo++){
+            int primaryDiagonl = matrix[rowNo][rowNo];
+            int secondaryDiagonal = matrix[rowNo][c-1-rowNo];
+            sum+=primaryDiagonl;
+            //some condition
+            if(rowNo != (c-1-rowNo)){
+                sum+=secondaryDiagonal; 
+            }
+        }
+        return sum;
+    }
+}
