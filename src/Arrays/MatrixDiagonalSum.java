6f18248003dbfd33702335012cd05d1f0097898c
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-27 23:48:01 +0530

    may 27

--- src/Arrays/MatrixDiagonalSum.java
+++ src/Arrays/MatrixDiagonalSum.java
@@ -5,17 +5,18 @@ public class MatrixDiagonalSum {
         int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         System.out.println(diagonalStudy(matrix));
     }
-    public static int diagonalStudy(int[][] matrix){
+
+    public static int diagonalStudy(int[][] matrix) {
         int r = matrix.length;
         int c = matrix[0].length;
         int sum = 0;
-        for(int rowNo = 0; rowNo < r; rowNo++){
+        for (int rowNo = 0; rowNo < r; rowNo++) {
             int primaryDiagonl = matrix[rowNo][rowNo];
-            int secondaryDiagonal = matrix[rowNo][c-1-rowNo];
-            sum+=primaryDiagonl;
-            //some condition
-            if(rowNo != (c-1-rowNo)){
-                sum+=secondaryDiagonal; 
+            int secondaryDiagonal = matrix[rowNo][c - 1 - rowNo];
+            sum += primaryDiagonl;
+            // some condition
+            if (rowNo != (c - 1 - rowNo)) {
+                sum += secondaryDiagonal;
             }
         }
         return sum;
