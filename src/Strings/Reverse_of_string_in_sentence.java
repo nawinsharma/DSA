fc6032cf04111db454578c252338be8c1a8874bc
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-05-30 17:44:40 +0530

    :sparkles: Reverse_of_string_in_sentence

---
+++ src/Strings/Reverse_of_string_in_sentence.java
@@ -0,0 +1,21 @@
+package Strings;
+
+public class Reverse_of_string_in_sentence {
+    public static void main(String[] args) {
+        String str = "hi my name is nawin";
+        System.out.println(reverseWords(str));
+
+    }
+
+    public static String reverseWords(String s) {
+        String[] str = s.trim().split("\\s+");
+        String out = "";
+        for (int i = str.length - 1; i > 0; i--) {
+            out += str[i] + " ";
+
+        }
+        return out + str[0];
+
+    }
+
+}
