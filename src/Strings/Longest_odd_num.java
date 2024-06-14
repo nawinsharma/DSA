12d875984199bb162c639b73c98235b12315573b
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-06-14 22:32:07 +0530

    :sparkles: add Longest odd number in a string

---
+++ src/Strings/Longest_odd_num.java
@@ -0,0 +1,34 @@
+package Strings;
+
+public class Longest_odd_num {
+    public static void main(String[] args) {
+        String x = "52";
+        System.out.println(largestOddNumber(x));
+    }
+
+    public static String largestOddNumber(String num) {
+        int n = num.length();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int digit = num.charAt(i) - '0';
+            if (digit % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+
+        return "";
+    }
+}
+
+// You are given a string num, representing a large integer. Return the
+// largest-valued odd integer (as a string) that is a non-empty substring of
+// num, or an empty string "" if no odd integer exists.
+
+// A substring is a contiguous sequence of characters within a string.
+
+// Example 1:
+
+// Input: num = "52"
+// Output: "5"
+// Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the
+// only odd number.
