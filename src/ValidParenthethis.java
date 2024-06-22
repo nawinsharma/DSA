62875d4145a63d60891a09bebd485a92f395e8a7
Nawin Kumar Sharma <nawinsharma60@gmail.com>
2024-06-22 08:42:55 +0530

    refactor

--- src/ValidParenthethis.java
+++ src/ValidParenthethis.java
@@ -5,16 +5,16 @@ public class ValidParenthethis {
         System.out.println(isValid("[]"));
 
     }
- 
+
     public static boolean isValid(String s) {
         // create a stack to store opening parentheses
         Stack<Character> stack = new Stack<>();
-        
+
         // loop through the string
         for (int i = 0; i < s.length(); i++) {
             // get the current character
             char c = s.charAt(i);
-            
+
             // if the current character is an opening parentheses
             if (c == '(' || c == '{' || c == '[') {
                 // push it onto the stack
@@ -28,7 +28,8 @@ public class ValidParenthethis {
                 }
                 // pop the top element from the stack
                 char top = stack.pop();
-                // if the current character and the top element from the stack don't match, return false
+                // if the current character and the top element from the stack don't match,
+                // return false
                 if (c == ')' && top != '(' || c == '}' && top != '{' || c == ']' && top != '[') {
                     return false;
                 }
