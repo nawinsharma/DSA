619c521a3029a87ebba9897be0d3e3dd50034619
Nawin <nawinsharma60@gmail.com>
2024-09-28 02:05:09 +0530

    chore: two sum -  hashmap

--- src/hashmap/TwoSum.java
+++ src/hashmap/TwoSum.java
@@ -1,21 +1,36 @@
 package hashmap;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
+
     public static void main(String[] args) {
-        int[] arr = {1,2,5,6};
-        System.out.println(Arrays.toString(BruteForce(arr, 11)));
-        
+        int[] arr = {1, 2, 5, 6};
+        System.out.println(Arrays.toString(Hashmap(arr, 11)));
     }
-    public static int[] BruteForce(int[] arr, int target){
-        for(int i = 0 ; i< arr.length; i++){
-            for(int j = i+1; j< arr.length; j++){
-                if(arr[i] + arr[j] == target){
+
+    public static int[] BruteForce(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
                     return new int[]{i, j};
                 }
             }
         }
-                return new int[]{};
+        return new int[]{};
+    }
+    public static int[] Hashmap(int[] arr, int target){
+        Map <Integer, Integer> map = new HashMap<>(); 
+        for(int i = 0 ; i< arr.length; i++){
+            int x = arr[i];
+            int y = target - x;
+            if(map.containsKey(y)){  // o(n) // if(map.get(y)!= null)
+                return new int[]{i, map.get(y)};
+            }
+            map.put(x, i);
+        }
+        return new int[]{};
     }
 }
