a6915d7f29d485ea078fc9311e00c7f608bbb9b7
nawinsh <nawinkumar@gamersberg.com>
2024-12-14 23:49:28 +0530

    update sun array

--- src/hashmap/Subarraywithsumk.java
+++ src/hashmap/Subarraywithsumk.java
@@ -11,22 +11,18 @@ public class Subarraywithsumk {
     }
 
     public static int hashmap_approach(int[] nums, int k) {
-        // Map to store (cumulative sum, frequency of the sum)
         HashMap<Integer, Integer> sumFrequencyMap = new HashMap<>();
-        sumFrequencyMap.put(0, 1); // To handle cases where the subarray starts from index 0
-
+        sumFrequencyMap.put(0, 1); 
         int cumulativeSum = 0;
         int count = 0;
 
         for (int num : nums) {
             cumulativeSum += num;
 
-            // Check if there's a subarray ending at this index with sum k
             if (sumFrequencyMap.containsKey(cumulativeSum - k)) {
                 count += sumFrequencyMap.get(cumulativeSum - k);
             }
 
-            // Update the frequency of the cumulative sum
             sumFrequencyMap.put(cumulativeSum, sumFrequencyMap.getOrDefault(cumulativeSum, 0) + 1);
         }
 
