980af5fe724d1e4ba683ccbe54de383e331fdd42
Nawin Sharma <126372969+nawinsharma@users.noreply.github.com>
2025-06-04 16:48:07 +0530

    Time: 20 ms (27.99%) | Memory: 59.6 MB (24.06%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for(int num : nums){
+            numSet.add(num);
+        }
+        return numSet.size() < nums.length;
+    }
+}
