38b7c68799e0720ddaad95fab7527f8949bd9e47
Nawin Sharma <126372969+nawinsharma@users.noreply.github.com>
2025-07-09 19:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.14%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,11 @@
 class Solution {
     public int fib(int n) {
-        if (n < 2) {
-            return n;
-        }
-        return fib(n - 1) + fib(n - 2);
+        // fuck it :)
+        return (int) Math.round(Math.pow((1 + Math.sqrt(5)) / 2, n) / Math.sqrt(5));
+
+        // if (n < 2) {
+        //     return n;
+        // }
+        // return fib(n - 1) + fib(n - 2);
     }
 }
