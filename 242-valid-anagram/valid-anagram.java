5078af3f05be3863d7fda1207fdcef5bdf159d2a
Nawin Sharma <126372969+nawinsharma@users.noreply.github.com>
2025-07-18 17:01:12 +0530

    Time: 8 ms (32.52%) | Memory: 42.7 MB (97.58%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
+
         int[] char_count = new int[26];
-        for(int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             char_count[s.charAt(i) - 'a']++;
             char_count[t.charAt(i) - 'a']--;
+
         }
-        for(int count : char_count){
-            if(count != 0){
+        for (int ch : char_count) {
+            if (ch != 0) {
                 return false;
             }
         }
-            return true;
+        return true;
     }
 }
+
