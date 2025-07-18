58437d35004e342736d2ecc2f17c39756e9dd18b
Nawin Sharma <126372969+nawinsharma@users.noreply.github.com>
2025-07-18 17:01:53 +0530

    Time: 5 ms (56.47%) | Memory: 43 MB (81.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,19 +3,16 @@ class Solution {
         if (s.length() != t.length()) {
             return false;
         }
-
         int[] char_count = new int[26];
         for (int i = 0; i < s.length(); i++) {
             char_count[s.charAt(i) - 'a']++;
             char_count[t.charAt(i) - 'a']--;
-
         }
-        for (int ch : char_count) {
-            if (ch != 0) {
+        for (int count : char_count) {
+            if (count != 0) {
                 return false;
             }
         }
         return true;
     }
 }
-
