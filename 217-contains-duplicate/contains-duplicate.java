3da17755ea389fad588a9ccb652a132e134b3199
Nawin Sharma <126372969+nawinsharma@users.noreply.github.com>
2025-07-21 03:29:02 +0530

    Time: 19 ms (36.47%) | Memory: 59.9 MB (20.67%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,31 +1,32 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        if(nums.length<2){
-            return false;
-        }
-    //     Arrays.sort(nums);
-    //     for(int i=1; i<nums.length;i++){
-    //         if(nums[i]==nums[i-1]){
-    //             return true;
-    //         }
-    //     }    
-    //     return false;
-    
-    Set<Integer> newSet = new HashSet<>();
-    for(int num: nums){
-        if(newSet.contains(num)){
-            return true;
-        }
-        newSet.add(num);
-    }
-    return false;
-    //o(n)
 
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        return  set.size() < nums.length;
 
-    // Set<Integer> numSet = new HashSet<>();
-    // for(int num:nums){
-    //     numSet.add(num);
-    // }
-    // return numSet.size() < nums.length;
+        // if(nums.length<2){
+        //     return false;
+        // }
+        //     Arrays.sort(nums);
+        //     for(int i=1; i<nums.length;i++){
+        //         if(nums[i]==nums[i-1]){
+        //             return true;
+        //         }
+        //     }    
+        //     return false;
+
+        // Set<Integer> newSet = new HashSet<>();
+        // for(int num: nums){
+        //     if(newSet.contains(num)){
+        //         return true;
+        //     }
+        //     newSet.add(num);
+        // }
+        // return false;
+        //o(n)
+
     }
 }
